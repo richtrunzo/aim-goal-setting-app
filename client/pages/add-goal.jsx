74ead1e322bad6dd9b998cbe29692d31dee16e9e
richtrunzo <richtrunzo@gmail.com>
2021-02-03 19:31:25 +0000

    Updated add entry JSX to render image to page and save it in state. #2.

--- client/pages/add-goal.jsx
+++ client/pages/add-goal.jsx
@@ -4,20 +4,38 @@ export default class Addgoal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      modal: false
+      modal: false,
+      image: null
     };
     this.modalOn = this.modalOn.bind(this);
     this.modalOff = this.modalOff.bind(this);
     this.modalRender = this.modalRender.bind(this);
     this.page = this.page.bind(this);
+    this.imageRender = this.imageRender.bind(this);
   }
 
   modalOn() {
-    this.setState({ modal: true });
+    this.setState({
+      modal: true,
+      image: this.state.image
+    });
   }
 
   modalOff() {
-    this.setState({ modal: false });
+    this.setState({
+      modal: false,
+      image: this.state.image
+    });
+  }
+
+  imageRender() {
+    if (this.state.modal === true) {
+      this.setState({
+        modal: this.state.modal,
+        image: event.target.className
+      });
+      console.log(this.state);
+    }
   }
 
   modalRender() {
@@ -39,58 +57,58 @@ export default class Addgoal extends React.Component {
             <div>
               <div className="d-flex justify-content-around mt-5">
                 <div>
-                  <i className="fas fa-dumbbell icon-one hover"></i>
+                  <i className="fas fa-dumbbell icon-one hover" onClick={this.imageRender}></i>
                 </div>
                 <div>
-                  <i className="fas fa-graduation-cap icon-one hover"></i>
+                  <i className="fas fa-graduation-cap icon-one hover" onClick={this.imageRender}></i>
                 </div>
                 <div>
-                  <i className="fas fa-book icon-one hover"></i>
+                  <i className="fas fa-book icon-one hover" onClick={this.imageRender}></i>
                 </div>
                 <div>
-                  <i className="fas fa-smoking-ban icon-one hover"></i>
+                  <i className="fas fa-smoking-ban icon-one hover" onClick={this.imageRender}></i>
                 </div>
               </div>
               <div className="d-flex justify-content-around mt-5">
                   <div>
-                    <i className="fas fa-running icon-one hover"></i>
+                    <i className="fas fa-running icon-one hover" onClick={this.imageRender}></i>
                   </div>
                   <div>
-                    <i className="fas fa-bed icon-one hover"></i>
+                    <i className="fas fa-bed icon-one hover" onClick={this.imageRender}></i>
                   </div>
                   <div>
-                    <i className="fas fa-place-of-worship icon-one hover"></i>
+                    <i className="fas fa-place-of-worship icon-one hover" onClick={this.imageRender}></i>
                   </div>
                   <div>
-                    <i className="fas fa-carrot icon-one hover"></i>
+                    <i className="fas fa-carrot icon-one hover" onClick={this.imageRender}></i>
                   </div>
               </div>
               <div className="d-flex justify-content-around mt-5">
                     <div>
-                      <i className="fas fa-code icon-one hover"></i>
+                      <i className="fas fa-code icon-one hover" onClick={this.imageRender}></i>
                     </div>
                     <div>
-                      <i className="fab fa-github icon-one hover"></i>
+                      <i className="fab fa-github icon-one hover" onClick={this.imageRender}></i>
                     </div>
                     <div>
-                      <i className="fas fa-pray icon-one hover"></i>
+                      <i className="fas fa-pray icon-one hover" onClick={this.imageRender}></i>
                     </div>
                     <div>
-                      <i className="fas fa-money-bill-alt icon-one hover"></i>
+                      <i className="fas fa-money-bill-alt icon-one hover" onClick={this.imageRender}></i>
                     </div>
               </div>
               <div className="d-flex justify-content-around mt-5">
                     <div>
-                      <i className="fas fa-calendar-check icon-one hover"></i>
+                      <i className="fas fa-calendar-check icon-one hover" onClick={this.imageRender}></i>
                     </div>
                     <div>
-                      <i className="fas fa-capsules icon-one hover"></i>
+                      <i className="fas fa-capsules icon-one hover" onClick={this.imageRender}></i>
                     </div>
                     <div>
-                      <i className="fas fa-futbol icon-one hover"></i>
+                      <i className="fas fa-futbol icon-one hover" onClick={this.imageRender}></i>
                     </div>
                     <div>
-                      <i className="fas fa-guitar icon-one hover"></i>
+                      <i className="fas fa-guitar icon-one hover" onClick={this.imageRender}></i>
                     </div>
               </div>
             </div>
@@ -103,7 +121,24 @@ export default class Addgoal extends React.Component {
   }
 
   page() {
-    return <div>
+    if (this.state.image !== null) {
+      return <div>
+              <div className="input-group input-group-lg mt-5">
+                <span className="input-group-text orange" id="inputGroup-sizing-md">Name Your Goal</span>
+                <input type="text" className="form-control white" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" />
+              </div>
+              <div className="d-grid gap-2 col-6 mx-auto">
+                <button className="btn btn-primary orange mt-5" type="button" onClick={this.modalOn}>Pick an Image</button>
+              </div>
+              <div className="col-6 mx-auto text-center mt-5">
+                <i className={this.state.image}></i>;
+              </div>
+              <div className="d-grid gap-2 col-6 mx-auto">
+                <button className="btn btn-primary orange mt-5" type="button">Save</button>
+              </div>
+            </div>;
+    } else {
+      return <div>
             <div className="input-group input-group-lg mt-5">
               <span className="input-group-text orange" id="inputGroup-sizing-md">Name Your Goal</span>
               <input type="text" className="form-control white" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" />
@@ -115,6 +150,7 @@ export default class Addgoal extends React.Component {
               <button className="btn btn-primary orange mt-5" type="button">Save</button>
             </div>
           </div>;
+    }
   }
 
   render() {
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -6,7 +6,7 @@ export default function Home(props) {
       <div className="d-flex justify-content-between flex-wrap">
         <div className="mt-5 col-6">
           <div className="mx-auto circle white border border-dark border-3">
-            <i className="icon-one position-relative top-50 start-50 translate-middle fas fa-plus"><a href="#addgoal"></a></i>
+            <a href="#addgoal"><i className="icon-one position-relative top-50 start-50 translate-middle fas fa-plus"></i></a>
           </div>
         </div>
      </div>
