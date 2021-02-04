c5a57f215770d02c8ddc91ee1baea9b34e4de7c8
richtrunzo <richtrunzo@gmail.com>
2021-02-04 01:50:54 +0000

    Updated home JSX to render saved goals to the page on componentdidmount. #3

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -23,8 +23,9 @@ export default class Home extends React.Component {
         console.log(this.state);
       });
   }
+
   noGoalsRender() {
-    return <div>
+    return (<div>
             <div className="d-flex justify-content-between flex-wrap">
               <div className="mt-5 col-6">
                 <div className="mx-auto circle white border border-dark border-3">
@@ -33,47 +34,36 @@ export default class Home extends React.Component {
               </div>
             </div>
           </div>
+    );
   }
+
   goalsRender() {
-    return this.state.goals.map((value, index) => {
-      <div>
-      <div className="d-flex justify-content-between flex-wrap">
-        <div className="mt-5 col-6">
-          <div className="mx-auto circle white border border-dark border-3">
-            <a href="#addgoal"><i className="icon-one position-relative top-50 start-50 translate-middle fas fa-plus"></i></a>
-          </div>
-        </div>
-     </div>
-    </div>
+    return (
       <div>
         <div className="d-flex justify-content-between flex-wrap">
-          <div className="mt-5 col-6">
-            <div className="mx-auto circle white border border-dark border-3">
-              <a href="#addgoal"><i className="icon-one position-relative top-50 start-50 translate-middle fas fa-plus"></i></a>
+          {this.state.goals.map((value, index) => {
+            return <div className="mt-5 col-6">
+                    <div className="mx-auto circle white border border-dark border-3">
+                      <i className={`icon-one position-relative top-50 start-50 translate-middle ${value.image}`}></i>
+                    </div>
+                    <p className="text-center text-two">{value.goalName}</p>
+                  </div>;
+          })}
+            <div className="mt-5 col-6">
+              <div className="mx-auto circle white border border-dark border-3">
+                <a href="#addgoal"><i className="icon-one position-relative top-50 start-50 translate-middle fas fa-plus"></i></a>
+              </div>
             </div>
-          </div>
-        </div>
       </div>
-
-    })
+    </div>
+    );
   }
 
   render() {
-    return (
-    <div>
-      <div className="d-flex justify-content-between flex-wrap">
-        <div className="mt-5 col-6">
-          <div className="mx-auto circle white border border-dark border-3">
-            <a href="#addgoal"><i className="icon-one position-relative top-50 start-50 translate-middle fas fa-plus"></i></a>
-          </div>
-        </div>
-        <div className="mt-5 col-6">
-          <div className="mx-auto circle white border border-dark border-3">
-            <a href="#addgoal"><i className="icon-one position-relative top-50 start-50 translate-middle fas fa-plus"></i></a>
-          </div>
-        </div>
-     </div>
-    </div>
-    );
+    if (this.state.goals.length > 0) {
+      return this.goalsRender();
+    } else {
+      return this.noGoalsRender();
+    }
   }
 }
