19b7b1d15b46ac31e5b413160859776fd2d7833a
richtrunzo <richtrunzo@gmail.com>
2021-02-08 23:45:43 +0000

    Added a component to render total goals completed. #8.

---
+++ client/components/all-goals.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default class Allgoals extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      goalcount: null
+    };
+  }
+
+  componentDidMount() {
+    const user = JSON.parse(localStorage.getItem('user-information'));
+    const userId = parseInt(user.userId);
+    fetch(`/api/goals/${userId}`, {
+      method: 'GET'
+    })
+      .then(res => res.json())
+      .then(data => {
+        let count = 0;
+        for (let i = 0; i < data.length; i++) {
+          count += data[i].goalCount;
+        }
+        this.setState({
+          goalcount: count
+        });
+      });
+  }
+
+  render() {
+    return <div>
+              <p className="text-two text-center mt-5 orange">Total goals completed: {this.state.goalcount}</p>
+          </div>;
+  }
+}
