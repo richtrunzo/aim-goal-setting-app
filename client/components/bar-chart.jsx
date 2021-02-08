9ffa584958a5639d6e35120959ed4752646198de
richtrunzo <richtrunzo@gmail.com>
2021-02-08 20:52:26 +0000

    Added backend data into bar chart. #8.

--- client/components/bar-chart.jsx
+++ client/components/bar-chart.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
 
-const data = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+const datachart = {
+  labels: [],
   datasets: [
     {
-      label: 'My First dataset',
+      label: 'My Goals',
       backgroundColor: 'rgba(255,99,132,0.2)',
       borderColor: 'rgba(255,99,132,1)',
       borderWidth: 1,
       hoverBackgroundColor: 'rgba(255,99,132,0.4)',
       hoverBorderColor: 'rgba(255,99,132,1)',
-      data: [65, 59, 80, 81, 56, 55, 40]
+      data: []
     }
   ]
 };
@@ -20,12 +20,48 @@ export default class Bargraph extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: null
+      goals: [],
+      data: {
+        labels: [],
+        datasets: [
+          {
+            label: 'My Goals',
+            backgroundColor: 'rgba(255,99,132,0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+            hoverBorderColor: 'rgba(255,99,132,1)',
+            data: []
+          }
+        ]
+      }
     };
   }
 
   componentDidMount() {
-    this.setState({ data: data });
+    const user = JSON.parse(localStorage.getItem('user-information'));
+    const userId = parseInt(user.userId);
+    fetch(`/api/goals/${userId}`, {
+      method: 'GET'
+    })
+      .then(res => res.json())
+      .then(data => {
+        const arr = [...this.state.goals];
+        for (let i = 0; i < data.length; i++) {
+          arr.push(data[i]);
+          datachart.labels.push(data[i].goalName);
+          datachart.datasets[0].data.push(data[i].goalCount);
+        }
+        this.setState({
+          goals: arr,
+          data: datachart
+        });
+      });
+  }
+
+  componentWillUnmount() {
+    datachart.labels = [];
+    datachart.datasets[0].data = [];
   }
 
   render() {
