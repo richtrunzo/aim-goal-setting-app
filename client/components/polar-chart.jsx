13cc58a3969ad8c963d141d29ab347414eb93503
richtrunzo <richtrunzo@gmail.com>
2021-02-08 22:48:17 +0000

    Updated polar chart with data from fetch request. #8

--- client/components/polar-chart.jsx
+++ client/components/polar-chart.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import { Polar } from 'react-chartjs-2';
 
-const data = {
+const datachart = {
   datasets: [{
-    data: [
-      11,
-      16,
-      7,
-      3,
-      14
-    ],
+    data: [],
     backgroundColor: [
       '#FF6384',
       '#4BC0C0',
@@ -17,27 +11,39 @@ const data = {
       '#E7E9ED',
       '#36A2EB'
     ],
-    label: 'My dataset' // for legend
+    label: 'My goals'
   }],
-  labels: [
-    'Red',
-    'Green',
-    'Yellow',
-    'Grey',
-    'Blue'
-  ]
+  labels: []
 };
 
 export default class Polarchart extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      goals: [],
       data: null
     };
   }
 
   componentDidMount() {
-    this.setState({ data: data });
+    const user = JSON.parse(localStorage.getItem('user-information'));
+    const userId = parseInt(user.userId);
+    fetch(`/api/goals/${userId}`, {
+      method: 'GET'
+    })
+      .then(res => res.json())
+      .then(data => {
+        const arr = [...this.state.goals];
+        for (let i = 0; i < data.length; i++) {
+          arr.push(data[i]);
+          datachart.labels.push(data[i].goalName);
+          datachart.datasets[0].data.push(data[i].goalCount);
+        }
+        this.setState({
+          goals: arr,
+          data: datachart
+        });
+      });
   }
 
   render() {
